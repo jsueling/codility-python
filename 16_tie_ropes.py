29822627515a7f6f345a3c5b1195928099b526f4
James Sue-Ling <james_sueling@hotmail.co.uk>
2025-11-16 15:51:39 +0000

    update Lesson 16 greedy algorithms

--- 16_tie_ropes.py
+++ 16_tie_ropes.py
@@ -4,7 +4,7 @@
 # O(N) for single pass through rope lengths.
 # Overall O(N)
 
-def solution(K, A):
+def solution(k: int, a: list) -> int:
     """
     Returns the maximum number of ropes of length at least K length
     that can be tied together (infinite ties, adjacent ropes only),
@@ -17,12 +17,11 @@ def solution(K, A):
     # Example with K = 5, A = [1, 5, ...]
     # At index 1 we have maximum 1 rope, including the 1 or not
 
-    n = len(A)
     accumulated_length = 0
     ropes = 0
-    for length in A:
+    for length in a:
         accumulated_length += length
-        if accumulated_length >= K:
+        if accumulated_length >= k:
             ropes += 1
             accumulated_length = 0
     return ropes
